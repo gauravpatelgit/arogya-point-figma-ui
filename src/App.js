a68177b24c94980646d57ab5abafa93342155c2f
ankitpatel5364-oss <ankitpatel5364@gmail.com>
2026-06-07 15:52:03 +0530

    my latest changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
+import logo from './logo.svg';
 import './App.css';
-import Products from './components/ProductList/index';
 
 function App() {
   return (
-  <div>
-    <Products/>
-  </div>
-
-  
+  <div>hiiiiiiiii</div>
   );
 }
 
--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import products from "../constant-data/products";
-console.log(products)
 function Index() {
+
     
-  return <div>hiiiii</div>;
+  return <div className="h-10">
+    
+  </div>;
 }
 
 export default Index;
